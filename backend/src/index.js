85416937b59131a4a03e7555d89b73f35af53f4a
Luis Enrique MP <luyo.lemp@gmail.com>
2020-01-18 16:26:57 -0300

    using cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 
@@ -9,6 +10,7 @@ mongoose.connect('url for mongo_database',{
   useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json()) //dando a entender ao express que as requisições serão no formato JSON
 app.use(routes)
 
